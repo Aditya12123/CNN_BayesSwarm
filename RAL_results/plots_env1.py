53a8434edfafe258e829212b4081c4f07fb6ff54
Aditya Divyakant Bhatt <abhatt4@buffalo.edu>
2024-08-21 17:49:34 -0400

    modified_README

--- RAL_results/plots_env1.py
+++ RAL_results/plots_env1.py
@@ -2,24 +2,43 @@ import os
 import numpy as np
 import pandas as pd
 import seaborn as sns
+os.environ['MPLCONFIGDIR'] = '/data1/users/abhatt4'
 import matplotlib.pyplot as plt
 plt.rcParams["font.family"] = "Arial"
 
 
 # frequency 1
-path = r'F:\\ADAMS_Lab\\RAL_results\\Env_1\\freq_5'
+# path = r'F:\\ADAMS_Lab\\RAL_results\\Env_1\\freq_5' # Windows_path
+path = "/data1/users/abhatt4/cnn_bayesswarm/CNN_BayesSwarm_RAL/RAL_results/Env_1/freq_1"
 
+# Windows Paths
+# folder_paths = [
+#                 # r"\\complete\\mission_time_",
+#                 r"\\cnn\\mission_time_",
+#                 r"\\src\\mission_time_",
+#                 r"\\kmeans\\mission_time_",
+#                 r"\\ransac\\mission_time_",
+# ]
+
+# Lab machine path
 folder_paths = [
-                # r"\\complete\\mission_time_",
-                r"\\cnn\\mission_time_",
-                r"\\src\\mission_time_",
-                r"\\kmeans\\mission_time_",
-                r"\\ransac\\mission_time_",
+                # "/complete/mission_time_",
+                "/cnn/mission_time_",
+                "/src/mission_time_",
+                "/kmeans/mission_time_",
+                "/ransac/mission_time_",
 ]
 
+
 mission = '850.csv'
-env_path = r"env1_results\\freq_5\\"
-file_name = r"env1_50_robots_speed1.pdf"
+# Windows Paths
+# env_path = r"env1_results\\freq_5\\"
+# file_name = r"env1_50_robots_speed1.pdf"
+
+#Lab machine 
+env_path = "env1_results/"#freq_5/"
+file_name = "env1_50_robots_speed1.png"
+
 
 fig, axs = plt.subplots(figsize=(6, 5.5))
 
@@ -33,7 +52,11 @@ for i, folder in enumerate(folder_paths):
     # arr = np.delete(arr, np.argmax(arr))
     # arr = np.delete(arr, np.argmax(arr))
     df = pd.DataFrame(arr)
-    df['Category'] = folder.split('\\')[2]
+    # windows
+    # df['Category'] = folder.split('\\')[2]
+
+    # Lab machine
+    df['Category'] = folder.split('/')[1] 
     data_list.append(df)
 
 ax = plt.subplot()
@@ -61,7 +84,11 @@ for tick in ax.yaxis.get_major_ticks():
     
 
 plt.tight_layout()
-save_image_path = r"F:\\ADAMS_Lab\\RAL_results\\results_plots\\" + env_path
-plt.savefig(save_image_path + file_name, format='pdf', dpi=300, pad_inches=1)
+# Windows path
+# save_image_path = r"F:\\ADAMS_Lab\\RAL_results\\results_plots\\" + env_path
+
+# Lab machine path
+save_image_path = "/data1/users/abhatt4/cnn_bayesswarm/CNN_BayesSwarm_RAL/RAL_results/results_plots/png_plots/" + env_path
+plt.savefig(save_image_path + file_name, format='png', dpi=300, pad_inches=1)
 plt.show()
 
